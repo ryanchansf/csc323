715067f30a261ecc09160ff7a8e3e59e265189ac
Ryan Chan <ryanchansf@gmail.com>
2024-02-06 10:09:06 -0800

    add starter code

---
+++ lab1/MT19937.py
@@ -0,0 +1,17 @@
+#Mersenne Twister MT 19937
+class MT19937:
+	def __init__(self, seed):
+		#TODO: Initialize MT state here
+		return
+
+	#Extract a tempered value based on MT[index]
+	#calling twist() every n numbers
+	def extract_number(self):
+		#TODO: Temper and Extract Here
+		return 42
+
+	#Generate the next n values from the series x_i 
+	def twist(self):
+		#TODO: Mix state here
+		return
+
